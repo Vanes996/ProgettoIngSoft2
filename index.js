8ea7708537df5761fce2e398f25ec99a68801174
BazzaLuca <lucabazza123@gmail.com>
2017-11-26 17:41:53 +0100

    ora abbiamo un database i topic sono le collections del database, dentro ogni topic le domande sono in JSON

--- index.js
+++ index.js
@@ -7,11 +7,19 @@
 
 var express = require('express');
 var apiai = require('apiai');
+// 26/11 aggiorno per il database
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://LucaBazza:test01@ds121716.mlab.com:21716/database_topic');
 
 var app = express();
 // client access token da cambiare con il mio
 var dia = apiai('c22a4fe6d883458e8063bc34327996d5');
 
+
+// Quello che sto per fare è un po una porcata a non so come altro fare
+var ISEE_Question = require("./ISEE_Question.js");
+var Laurea_Question = require("./Laurea_Question.js");
+var Piano_Question = require("./Piano_Question.js");
 
 //handle get req on /question
 app.get('/question', function (req, res) {    
@@ -30,6 +38,9 @@ app.get('/question', function (req, res) {
     });
     // OCCHIO AL CALLBACK HELL BISOGNA ASSICURARSI CHE QUESTO FINISCA PRIMA
 
+    // HO PROVATO A CREARE A MANO LE DOMANDE E QUANDO FA IL FIND NON LE TROVA
+    // ALLA PEGGIO LE INSERIAMO TUTTE VIA CODICE
+    // IL CODDICE CHE HO COMMMENTATO SERVE A QUELLO
     request.on('response', function(response) {
     	 // Devo prendere la risposta vera e propria
          // 24/11 Sostituito l'agent placeholder con il io funziona
@@ -38,16 +49,44 @@ app.get('/question', function (req, res) {
 
          // cercare la risposta nel db
          if (ris == "LAUREA") {
-            console.log("cercherò nel database le risposte riguardanti la LAUREA");
+            console.log("cercherò nel database le risposte riguardanti la Laurea");
+            /*var domanda = new Laurea_Question();
+            domanda.value = "questa è la prima domanda";
+            domanda.answer = "questa è la risposta alla prima domanda";
+            domanda.rating = "1 è una stringa dopo devo cambiare";
+            domanda.save(function(err) {
+                if (err) {
+                    console.log("errore");
+                }
+                console.log("domanda creata in che collection finirà?");
+            });*/
+            Laurea_Question.find(function(err, questions) {
+                if (err) {
+                    console.log(err);
+                }
+                console.log(questions);
+            });
          }
          else if (ris == "PIANO") {
             console.log("cercherò nel database le risposte riguardanti il PIANO");
+            Piano_Question.find(function(err, questions) {
+                if (err) {
+                    console.log(err);
+                }
+                console.log(questions);
+            });
          }
          else if (ris == "ISEE") {
             console.log("cercherò nel database le risposte riguardanti l'ISEE");
+            ISEE_Question.find(function(err, questions) {
+                if (err) {
+                    console.log(err);
+                }
+                console.log(questions);
+            });
          }
          else {
-            console.log("non ci arriva mai in teoria ma lo metto per il futuro");
+            console.log("non ci arriva mai in teoria ma lo metto per il futuro"); 
          }
 
 	});
